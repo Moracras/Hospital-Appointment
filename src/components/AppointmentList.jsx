d5cd9e42052cf5cac65310ff1f712ddf6b3b0462
Moracras <tavfikkarademir@gmail.com>
2023-12-11 22:30:21 +0300

    data

--- src/components/AppointmentList.jsx
+++ src/components/AppointmentList.jsx
@@ -1,13 +1,33 @@
-import Container from "react-bootstrap/Container"
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/esm/Col";
+import Row from "react-bootstrap/esm/Row";
+import { TiDelete } from "react-icons/ti";
 
-const AppointmentList = () => {
+const AppointmentList = ({ appointments }) => {
   return (
     <Container className="p-2">
       <h3 className="display-6 mb-2" style={{ color: "rgb(166, 18, 189)" }}>
         Appointment List
       </h3>
+      {appointments.map(({ id, patient, consulted, doctor, day }) => (
+        <div key={id} className="appointments">
+          <Row>
+            <Col>
+              <h4>{patient} </h4>
+              <h5>{doctor} </h5>
+            </Col>
+            <Col>
+              <h5>Date: {day.toLocaleDateString("tr")} </h5>
+              <h6>Time: {day.toLocaleTimeString("tr")} </h6>
+            </Col>
+            <Col>
+              <TiDelete className="text-danger fs-1" type="button" />
+            </Col>
+          </Row>
+        </div>
+      ))}
     </Container>
-  )
-}
+  );
+};
 
-export default AppointmentList
+export default AppointmentList;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import { useState } from "react";
 import Doctors from '../components/Doctors'
-import { Container } from 'react-bootstrap'
 import AppointmentList from '../components/AppointmentList'
+import { appointmentData } from "../helpers/Data"
 
 const Home = () => {
+  const [appointments, setAppointments] = useState(appointmentData);
   return (
     <main className="text-center mt-2">
       <h1 className="display-5 text-danger">CLARUS HOSPITAL</h1>
       <Doctors />
-      <AppointmentList />
+      <AppointmentList appointments={appointments} />
     </main>
   )
 }
